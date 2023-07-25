9dff389b1c1c4db057ccbf24ac6b5d1c687f9ed7
Sajad <sajadrahimi0101@gmail.com>
2023-07-25 13:34:03 +0330

    add some lotties json file for home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 import 'package:rayanik/core/widgets/appbar_widget.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -21,14 +23,61 @@ class HomeScreen extends StatelessWidget {
               height: Get.height / 4,
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15),
-                  image:const DecorationImage(
+                  image: const DecorationImage(
                       fit: BoxFit.fill,
                       image: AssetImage(
                         "assets/pictures/images/banner.png",
                       ))),
-            )
-         
-            ,Expanded(child: GridView.builder(gridDelegate: gridDelegate, itemBuilder: itemBuilder))
+            ),
+            SizedBox(height: Get.height / 18),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              SizedBox(
+                  width: Get.width / 4,
+                  // height: Get.height / 10,
+                  child: Lottie.asset(
+                    "assets/pictures/icons/rocket.json",
+                  )),
+              SizedBox(
+                  width: Get.width / 3,
+                  // height: Get.height / 10,
+                  child: Lottie.asset(
+                    "assets/pictures/icons/learning.json",
+                  )),
+              SizedBox(
+                  width: Get.width / 3,
+                  // height: Get.height / 10,
+                  child: Lottie.asset(
+                    "assets/pictures/icons/service.json",
+                  )),
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              SizedBox(
+                  width: Get.width / 2,
+                  // height: Get.height / 10,
+                  child: Lottie.asset(
+                    "assets/pictures/icons/aboutus.json",
+                  )),
+              SizedBox(
+                  width: Get.width / 3,
+                  // height: Get.height / 10,
+                  child: Lottie.asset(
+                    "assets/pictures/icons/contact.json",
+                  )),
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              SizedBox(
+                  width: Get.width / 3,
+                  // height: Get.height / 10,
+                  child: Lottie.asset(
+                    "assets/pictures/icons/collabration.json",
+                  )),
+              SizedBox(
+                  width: Get.width / 4,
+                  // height: Get.height / 10,
+                  child: Lottie.asset(
+                    "assets/pictures/icons/jobs.json",
+                  )),
+            ])
           ],
         ),
       ),
