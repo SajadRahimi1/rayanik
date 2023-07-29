1c1378c2c4dcce4a3bfcca971954311cf5bcc80b
Sajad <sajadrahimi0101@gmail.com>
2023-07-29 13:02:35 +0330

    podcast screen UI done

--- lib/screens/learning/learning_menu_screen.dart
+++ lib/screens/learning/learning_menu_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:rayanik/core/widgets/appbar_widget.dart';
 import 'package:rayanik/screens/learning/book_learning_screen.dart';
+import 'package:rayanik/screens/learning/podcast_learning_screen.dart';
 import 'package:rayanik/screens/learning/video_learning_screen.dart';
 
 class LearningMenuScreen extends StatelessWidget {
@@ -43,28 +44,32 @@ class LearningMenuScreen extends StatelessWidget {
         ),
 
         // podcast
-        Container(
-          margin: EdgeInsets.all(10),
-          width: Get.width,
-          height: Get.height / 4,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Color(0xffe9bcac)),
-          child: Row(
-            children: [
-              Expanded(
-                  child: Text(
-                "پادکست",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Color(0xffffffff), fontSize: Get.width / 16),
-              )),
-              SizedBox(
-                height: Get.height,
-                width: Get.width / 1.8,
-                child: Image.asset("assets/pictures/images/podcast.png"),
-              )
-            ],
+        InkWell(
+          onTap: () => Get.to(() => const PodcastLearningScreen(),
+              transition: Transition.leftToRight),
+          child: Container(
+            margin:const EdgeInsets.all(10),
+            width: Get.width,
+            height: Get.height / 4,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color:const Color(0xffe9bcac)),
+            child: Row(
+              children: [
+                Expanded(
+                    child: Text(
+                  "پادکست",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Color(0xffffffff), fontSize: Get.width / 16),
+                )),
+                SizedBox(
+                  height: Get.height,
+                  width: Get.width / 1.8,
+                  child: Image.asset("assets/pictures/images/podcast.png"),
+                )
+              ],
+            ),
           ),
         ),
 
