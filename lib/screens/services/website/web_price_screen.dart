d1cc11fe2f51a835cb1a54f7b4413fe1b98764f6
Sajad <sajadrahimi0101@gmail.com>
2023-08-12 15:12:34 +0330

    company plan data entered

--- lib/screens/services/website/web_price_screen.dart
+++ lib/screens/services/website/web_price_screen.dart
@@ -4,7 +4,8 @@ import 'package:rayanik/core/widgets/appbar_widget.dart';
 import 'package:rayanik/screens/services/website/web_plans_screen.dart';
 
 class WebPriceScreen extends StatelessWidget {
-  const WebPriceScreen({Key? key}) : super(key: key);
+  const WebPriceScreen({Key? key, required this.isShop}) : super(key: key);
+  final bool isShop;
 
   @override
   Widget build(BuildContext context) {
@@ -19,41 +20,74 @@ class WebPriceScreen extends StatelessWidget {
                   height: 15,
                 ),
                 TabBar(
+                    indicatorColor: const Color(0xffbc8971),
                     labelColor: Colors.black,
                     tabs: List.generate(
                         5, (index) => Tab(text: "پلن ${index + 1}"))),
                 Expanded(
                     child: TabBarView(
                   physics: const NeverScrollableScrollPhysics(),
-                  children: [
-                    WebPlansScreen(
-                        planTitle: "پلن حمایتی",
-                        items: plan1.split('\n'),
-                        price: "7,000,000 تومان"),
-                    WebPlansScreen(
-                      planTitle: "پلن اقتصادی",
-                      items: plan2.split('\n'),
-                      price: "10,000,000 تومان",
-                      lastEnableIndex: 27,
-                    ),
-                    WebPlansScreen(
-                      planTitle: "پلن استاندارد",
-                      items: plan3.split('\n'),
-                      price: "14,000,000 تومان",
-                      lastEnableIndex: 35,
-                    ),
-                    WebPlansScreen(
-                      planTitle: "پلن ویژه",
-                      items: plan4.split('\n'),
-                      price: "19,000,000 تومان",
-                      lastEnableIndex: 43,
-                    ),
-                    WebPlansScreen(
-                      planTitle: "پلن VIP",
-                      items: plan5.split('\n'),
-                      price: "25,000,000 تومان",
-                    ),
-                  ],
+                  children: isShop
+                      ? [
+                          WebPlansScreen(
+                              planTitle: "پلن حمایتی",
+                              items: plan1.split('\n'),
+                              price: "7,000,000 تومان",
+                              lastEnableIndex: 21),
+                          WebPlansScreen(
+                            planTitle: "پلن اقتصادی",
+                            items: plan2.split('\n'),
+                            price: "10,000,000 تومان",
+                            lastEnableIndex: 27,
+                          ),
+                          WebPlansScreen(
+                            planTitle: "پلن استاندارد",
+                            items: plan3.split('\n'),
+                            price: "14,000,000 تومان",
+                            lastEnableIndex: 35,
+                          ),
+                          WebPlansScreen(
+                            planTitle: "پلن ویژه",
+                            items: plan4.split('\n'),
+                            price: "19,000,000 تومان",
+                            lastEnableIndex: 43,
+                          ),
+                          WebPlansScreen(
+                            planTitle: "پلن VIP",
+                            items: plan5.split('\n'),
+                            price: "25,000,000 تومان",
+                          ),
+                        ]
+                      : [
+                          WebPlansScreen(
+                              planTitle: "پلن حمایتی",
+                              items: companyPlan1.split('\n'),
+                              price: "5,000,000 تومان",
+                              lastEnableIndex: 11),
+                          WebPlansScreen(
+                            planTitle: "پلن اقتصادی",
+                            items: companyPlan2.split('\n'),
+                            price: "8,000,000 تومان",
+                            lastEnableIndex: 16,
+                          ),
+                          WebPlansScreen(
+                            planTitle: "پلن استاندارد",
+                            items: companyPlan3.split('\n'),
+                            price: "10,000,000 تومان",
+                            lastEnableIndex: 20,
+                          ),
+                          WebPlansScreen(
+                            planTitle: "پلن ویژه",
+                            items: companyPlan4.split('\n'),
+                            price: "14,000,000 تومان",
+                            lastEnableIndex: 25,
+                          ),
+                          WebPlansScreen(
+                            planTitle: "پلن VIP",
+                            items: companyPlan5.split('\n'),
+                            price: "18,000,000 تومان",
+                          ),
+                        ],
                 ))
               ],
             )),
--- lib/screens/services/website/website_type_screen.dart
+++ lib/screens/services/website/website_type_screen.dart
@@ -12,7 +12,7 @@ class WebsiteTypeScreen extends StatelessWidget {
       appBar: screensAppBar(),
       body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         InkWell(
-          onTap: () => Get.to(() => const WebPriceScreen()),
+          onTap: () => Get.to(() => const WebPriceScreen(isShop: true,)),
           child: Container(
             margin: const EdgeInsets.all(10),
             width: Get.width,
@@ -42,29 +42,32 @@ class WebsiteTypeScreen extends StatelessWidget {
         SizedBox(
           height: Get.height / 10,
         ),
-        Container(
-          margin: const EdgeInsets.all(10),
-          width: Get.width,
-          height: Get.height / 4.5,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: const Color(0xffe9bcac)),
-          child: Row(
-            children: [
-              Expanded(
-                  child: Text(
-                "طراحی سایت شرکتی",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: const Color(0xffffffff),
-                    fontSize: 19 * MediaQuery.of(context).textScaleFactor),
-              )),
-              SizedBox(
-                height: Get.height,
-                width: Get.width / 2.5,
-                child: Image.asset("assets/pictures/images/web.png"),
-              )
-            ],
+         InkWell(
+          onTap: () => Get.to(() => const WebPriceScreen(isShop: false,)),
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            width: Get.width,
+            height: Get.height / 4.5,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: const Color(0xffe9bcac)),
+            child: Row(
+              children: [
+                Expanded(
+                    child: Text(
+                  "طراحی سایت شرکتی",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: const Color(0xffffffff),
+                      fontSize: 19 * MediaQuery.of(context).textScaleFactor),
+                )),
+                SizedBox(
+                  height: Get.height,
+                  width: Get.width / 2.5,
+                  child: Image.asset("assets/pictures/images/web.png"),
+                )
+              ],
+            ),
           ),
         ),
       ]),
